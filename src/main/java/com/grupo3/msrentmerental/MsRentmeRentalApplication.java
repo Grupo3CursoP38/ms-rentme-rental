3e535cf867eb7e811cd0a5736945c98febe9b7a2
g1alexander <alexlds26@gmail.com>
2021-11-19 20:54:05 -0500

    feat: init project

---
+++ src/main/java/com/grupo3/msrentmerental/MsRentmeRentalApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.msrentmerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRentmeRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRentmeRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo3/msrentmerental/MsRentmeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.msrentmerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRentmeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
